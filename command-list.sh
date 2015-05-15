d5f78a0740f8098ded2a4ba6f7d305cad2614cba
Kaoru Nasuno Local <kaorun423@gmail.com>
2015-05-15 14:11:41 +0900

    mod some scripts

--- command-list.sh
+++ command-list.sh
@@ -8,3 +8,6 @@ sudo apt-get install -y python2.7-dev python-pip python-matplotlib build-essenti
 sudo pip install MySQL-python networkx beautifulsoup4 nose pymc pandas virtualenv scikit-learn;
 sudo pip install --allow-external mysql-connector-python mysql-connector-python;
 sudo pip install --upgrade gensim;
+
+
+sh ./test-scripts/python-test.sh 2> _test.log;
