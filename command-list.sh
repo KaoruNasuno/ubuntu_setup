304acd286945c5374ead0dcb1c1c3b8e50affc01
Kaoru Nasuno Local <kaorun423@gmail.com>
2015-05-15 13:06:32 +0900

    add test.sh and mod commandlist

--- command-list.sh
+++ command-list.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y vim git byobu unzip debconf-utils curl libmysqlclient-de
 sudo apt-get install -y mailutils sasl2-bin;
 
 sudo apt-get install -y mariadb-server;
-sudo apt-get install -y python2.7-dev python-pip python-matplotlib build-essential python-dev python-numpy python-setuptools python-scipy libatlas-dev libatlas3-base ipython ipython-notebook;
+sudo apt-get install -y python2.7-dev python-pip python-matplotlib build-essential python-dev python-numpy python-setuptools python-scipy libatlas-dev libatlas3-base ipython ipython-notebook mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 python-mecab;
 sudo pip install MySQL-python networkx beautifulsoup4 nose pymc pandas virtualenv scikit-learn;
 sudo pip install --allow-external mysql-connector-python mysql-connector-python;
 sudo pip install --upgrade gensim;
---
+++ test.sh
@@ -0,0 +1,16 @@
+nosetests MySQLdb;
+nosetests mysql-connector;
+nosetests matplotlib;
+nosetests numpy;
+nosetests scipy;
+nosetests sklearn;
+nosetests gensim;
+nosetests bs4;
+nosetests pandas;
+
+nosetests json;
+nosetests urllib2;
+nosetests networkx;
+nosetests csv;
+nosetests pymc;
+nosetests MeCab;
